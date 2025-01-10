5cd38d74b7cff84ac51c13cd9ad436655aa320d4
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2025-01-10 22:27:29 +0100

    feat: Add support for version with a stage at the end

--- drivers/pterodactyl/build.rs
+++ drivers/pterodactyl/build.rs
@@ -5,11 +5,11 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    generate_build_info("Alpha");
+    generate_build_info();
     Ok(())
 }
 
-fn generate_build_info(stage: &str) {
+fn generate_build_info() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let mut file = File::create(format!("{}/build_info.rs", out_dir)).unwrap();
 
@@ -27,12 +27,12 @@ fn generate_build_info(stage: &str) {
     writeln!(file, "    patch: {},", version.2).unwrap();
     writeln!(file, "    build: {},", build).unwrap();
     writeln!(file, "    commit: \"{}\",", commit).unwrap();
-    writeln!(file, "    stage: Stage::{},", stage).unwrap();
+    writeln!(file, "    stage: Stage::{},", version.3).unwrap();
     writeln!(file, "    protocol: {},", protocol_version).unwrap();
     writeln!(file, "}};").unwrap();
 }
 
-fn get_version_info() -> Result<(u16, u16, u16), Box<dyn std::error::Error>> {
+fn get_version_info() -> Result<(u16, u16, u16, String), Box<dyn std::error::Error>> {
     let cargo_toml_content = fs::read_to_string("Cargo.toml")?;
     let cargo_toml: toml::Value = toml::from_str(&cargo_toml_content)?;
 
@@ -40,13 +40,19 @@ fn get_version_info() -> Result<(u16, u16, u16), Box<dyn std::error::Error>> {
         .as_str()
         .ok_or("Unable to get version from Cargo.toml")?;
 
-    let version_parts: Vec<u16> = version_str
+    let version_parts: Vec<&str> = version_str.split('-').collect();
+    let version_numbers: Vec<u16> = version_parts[0]
         .split('.')
         .map(|v| v.parse().map_err(|_| "Invalid version part"))
         .collect::<Result<Vec<_>, _>>()?;
 
-    if version_parts.len() == 3 {
-        Ok((version_parts[0], version_parts[1], version_parts[2]))
+    if version_numbers.len() == 3 {
+        let stage = if version_parts.len() > 1 {
+            version_parts[1][0..1].to_uppercase() + &version_parts[1][1..]
+        } else {
+            "Stable".to_string()
+        };
+        Ok((version_numbers[0], version_numbers[1], version_numbers[2], stage))
     } else {
         Err("Version must have three parts".into())
     }

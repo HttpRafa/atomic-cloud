4ac398ad0ecbbc94ca9b64c3d4bdf5c328aeaed0
HttpRafa <60099368+HttpRafa@users.noreply.github.com>
2025-03-28 19:17:14 +0100

    fix: Fix actions

--- clients/java/api/build.gradle.kts
+++ clients/java/api/build.gradle.kts
@@ -3,11 +3,25 @@ plugins {
 
     id("cloud-base")
     id("cloud-format")
+
+    id("maven-publish")
 }
 
-repositories {
-    mavenCentral()
-}
+publishing {
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 
-dependencies {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/HttpRafa/atomic-cloud")
+            name = "GitHubPackages"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
